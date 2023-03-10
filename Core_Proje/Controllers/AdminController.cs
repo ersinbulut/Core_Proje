4bf7a80b1353b2c8832274c41ef626e883f72e12
BULUTS <BULUTS@BULUTS>
2023-03-10 11:00:37 +0300

    46. Admin Layout Alanlarında Partial Düzenlemeleri

---
+++ Core_Proje/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+    }
+}
