99dd0b4e8991c7f32fbd17b5e2a80959beeed54c
BULUTS <BULUTS@BULUTS>
2023-03-13 13:22:32 +0300

    52. Yeteneklerin Güncellenmesi

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -11,6 +12,34 @@ namespace Core_Proje.Controllers
         {
             var values = skillManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skills)
+        {
+            skillManager.TAdd(skills);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var value = skillManager.TGetByID(id);
+            skillManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        public IActionResult EditSkill(int id)
+        {
+            var values = skillManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
